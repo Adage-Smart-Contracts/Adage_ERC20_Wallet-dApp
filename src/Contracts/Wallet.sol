f289865ab80a8aaeb7170b56465ba86d1bbf6182
Regan <36708505+indiansatoshi@users.noreply.github.com>
2022-05-31 17:15:28 +0530

    code upload

---
+++ src/Contracts/Wallet.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Adage_Token is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Adage", "ADAGE") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+}
